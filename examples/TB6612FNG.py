cbb601fee4478ef15400391fa13f4b3ffcd01722
Alex979 <alexbroaddus1@gmail.com>
2018-10-29 23:03:43 -0400

    Moved some files to examples folder

---
+++ examples/TB6612FNG.py
@@ -0,0 +1,36 @@
+from gpiozero import OutputDevice, PWMOutputDevice
+
+class TB6612FNG:
+
+    def __init__(self, IN1, IN2, PWM, STBY):
+        self.input_1 = OutputDevice(IN1)
+        self.input_2 = OutputDevice(IN2)
+        self.speed_control = PWMOutputDevice(PWM)
+        self.standby = OutputDevice(STBY)
+        
+        self.standby.on()
+        self.speed_control.value = 1
+    
+    def forward(self):
+        print('Motor forward')
+        self.input_1.on()
+        self.input_2.off()
+    
+    def backward(self):
+        print('Motor backward')
+        self.input_1.off()
+        self.input_2.on()
+    
+    def stop(self):
+        print('Motor stop')
+        self.input_1.off()
+        self.input_2.off()
+    
+    def brake(self):
+        print('Motor brake')
+        self.input_1.on()
+        self.input_2.on()
+
+    def set_speed(self, speed):
+        print('Setting motor speed to ' + str(speed))
+        self.speed_control.value = speed
