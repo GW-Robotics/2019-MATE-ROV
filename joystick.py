4ce6f025544c50b515d2150bf959447278319313
Rick Sear <yobob.sear@gmail.com>
2018-11-28 10:06:46 -0500

    Added button mappings to comments

--- joystick.py
+++ joystick.py
@@ -17,37 +17,42 @@ if(joystick_count > 1):
     print("More than 1 joystick found.")
 
 def query_values(joystick):    
-    # Usually axis run in pairs, up/down for one, and left/right for
-    # the other.
+
     axes = joystick.get_numaxes()
-    print("Number of axes: " + str(axes))
-    
+    # Values go from -1 to 1
+    # 0: LStick L/R
+    # 1: LStcik U/D
+    # 2: R2/L2
+    # 3: RStick U/D
+    # 4: RStick L/R
     for i in range(axes):
         axis = joystick.get_axis(i)
-        print("Axis "+str(i)+" value: "+str(axis))
         
+    buttons = joystick.get_numbuttons()
+    # Values are 0 or 1
+    # 0: A
+    # 1: B
+    # 2: X
+    # 3: Y
+    # 4: L1
+    # 5: R1
+    # 6: BACK
+    # 7: START
+    # 8: L3
+    # 9: R3
+    for i in range(buttons):
+        button = joystick.get_button(i)
         
-    # buttons = joystick.get_numbuttons()
-    # print("Number of buttons: {}".format(buttons) )
-        
-
-    # for i in range( buttons ):
-    #     button = joystick.get_button( i )
-    #     print("Button {:>2} value: {}".format(i,button) )
-        
-        
-    # # Hat switch. All or nothing for direction, not like joysticks.
-    # # Value comes back in an array.
-    # hats = joystick.get_numhats()
-    # print("Number of hats: {}".format(hats) )
-        
-
-    # for i in range( hats ):
-    #     hat = joystick.get_hat( i )
-    #     print("Hat {} value: {}".format(i, str(hat)) )
+    hats = joystick.get_numhats()
+    # Value comes back in a tuple (x, y) from with values -1, 0, or 1
+    # x: L/R
+    # y: D/U
+    for i in range(hats):
+        hat = joystick.get_hat(i)
 
 # Main loop
 button_vals = [0]*16
+# while True:
 for i in range(10000):
 
     the_joystick = pygame.joystick.Joystick(0)
