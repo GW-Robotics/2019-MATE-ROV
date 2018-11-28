4c639fd384ac347bfc62d3823d3b5de58a41c05d
Rick Sear <yobob.sear@gmail.com>
2018-11-28 09:45:53 -0500

    Cleaned up controller code

--- joystick.py
+++ joystick.py
@@ -1,64 +1,63 @@
 import pygame
 
+# Initialize pygame
 pygame.init()
 
 #Initialize joysticks
 pygame.joystick.init()
 
-button_vals = [0]*16
-
+# Acceptable events to query the controller
 input_type = [pygame.JOYAXISMOTION, pygame.JOYBALLMOTION, pygame.JOYBUTTONDOWN, pygame.JOYBUTTONUP, pygame.JOYHATMOTION]
 
 # Get count of joysticks
 joystick_count = pygame.joystick.get_count()
 
-#Main loop
-for hello in range(10000):
+# Alert user if more than 1
+if(joystick_count > 1):
+    print("More than 1 joystick found.")
 
-    # For each joystick:
-    for i in range(joystick_count):
-        curr_joystick = pygame.joystick.Joystick(i)
-        curr_joystick.init()
-        query_values(curr_joystick)
+def query_values(joystick):    
+    # Usually axis run in pairs, up/down for one, and left/right for
+    # the other.
+    axes = joystick.get_numaxes()
+    print("Number of axes: " + str(axes))
+    
+    for i in range(axes):
+        axis = joystick.get_axis(i)
+        print("Axis "+str(i)+" value: "+str(axis))
+        
+        
+    # buttons = joystick.get_numbuttons()
+    # print("Number of buttons: {}".format(buttons) )
+        
+
+    # for i in range( buttons ):
+    #     button = joystick.get_button( i )
+    #     print("Button {:>2} value: {}".format(i,button) )
+        
+        
+    # # Hat switch. All or nothing for direction, not like joysticks.
+    # # Value comes back in an array.
+    # hats = joystick.get_numhats()
+    # print("Number of hats: {}".format(hats) )
+        
+
+    # for i in range( hats ):
+    #     hat = joystick.get_hat( i )
+    #     print("Hat {} value: {}".format(i, str(hat)) )
+
+# Main loop
+button_vals = [0]*16
+for i in range(10000):
+
+    the_joystick = pygame.joystick.Joystick(0)
+    the_joystick.init()
+    query_values(the_joystick)
     
     # EVENT PROCESSING STEP
     for event in pygame.event.get(): # User did something
         if event.type in input_type: #if event was an input
             print("input lol")
 
-def query_values(joystick):    
-    # Usually axis run in pairs, up/down for one, and left/right for
-    # the other.
-    axes = joystick.get_numaxes()
-    print("Number of axes: {}".format(axes))
-    
-    for i in range(axes):
-        axis = joystick.get_axis( i )
-        print("Axis {} value: {:>6.3f}".format(i, axis) )
-        
-        
-    buttons = joystick.get_numbuttons()
-    print("Number of buttons: {}".format(buttons) )
-        
-
-    for i in range( buttons ):
-        button = joystick.get_button( i )
-        print("Button {:>2} value: {}".format(i,button) )
-        
-        
-    # Hat switch. All or nothing for direction, not like joysticks.
-    # Value comes back in an array.
-    hats = joystick.get_numhats()
-    print("Number of hats: {}".format(hats) )
-        
-
-    for i in range( hats ):
-        hat = joystick.get_hat( i )
-        print("Hat {} value: {}".format(i, str(hat)) )
-
-
-    
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
+# End pygame
 pygame.quit ()
