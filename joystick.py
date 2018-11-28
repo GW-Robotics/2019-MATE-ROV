01ba00967568eccd830ba4eb9e3ea2b61c59980a
Rick Sear <yobob.sear@gmail.com>
2018-11-27 21:55:22 -0500

    Progress on controller readout

--- joystick.py
+++ joystick.py
@@ -2,57 +2,59 @@ import pygame
 
 pygame.init()
 
-#Loop until the user clicks the close button.
-done = False
-
 #Initialize joysticks
 pygame.joystick.init()
 
+button_vals = [0]*16
+
+input_type = [pygame.JOYAXISMOTION, pygame.JOYBALLMOTION, pygame.JOYBUTTONDOWN, pygame.JOYBUTTONUP, pygame.JOYHATMOTION]
+
+# Get count of joysticks
+joystick_count = pygame.joystick.get_count()
+
 #Main loop
-while not done:
+for hello in range(10000):
+
+    # For each joystick:
+    for i in range(joystick_count):
+        curr_joystick = pygame.joystick.Joystick(i)
+        curr_joystick.init()
+        query_values(curr_joystick)
     
     # EVENT PROCESSING STEP
     for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True # Flag that we are done so we exit this loop
+        if event.type in input_type: #if event was an input
+            print("input lol")
 
-    # Get count of joysticks
-    joystick_count = pygame.joystick.get_count()
-    print("Joysticks: "+str(joystick_count))
+def query_values(joystick):    
+    # Usually axis run in pairs, up/down for one, and left/right for
+    # the other.
+    axes = joystick.get_numaxes()
+    print("Number of axes: {}".format(axes))
     
-    # For each joystick:
-    for i in range(joystick_count):
-        joystick = pygame.joystick.Joystick(i)
-        joystick.init()
+    for i in range(axes):
+        axis = joystick.get_axis( i )
+        print("Axis {} value: {:>6.3f}".format(i, axis) )
         
-        # Usually axis run in pairs, up/down for one, and left/right for
-        # the other.
-        axes = joystick.get_numaxes()
-        print("Number of axes: {}".format(axes))
         
-        for i in range(axes):
-            axis = joystick.get_axis( i )
-            print("Axis {} value: {:>6.3f}".format(i, axis) )
-         
-            
-        buttons = joystick.get_numbuttons()
-        print("Number of buttons: {}".format(buttons) )
-         
+    buttons = joystick.get_numbuttons()
+    print("Number of buttons: {}".format(buttons) )
+        
 
-        for i in range( buttons ):
-            button = joystick.get_button( i )
-            print("Button {:>2} value: {}".format(i,button) )
-         
-            
-        # Hat switch. All or nothing for direction, not like joysticks.
-        # Value comes back in an array.
-        hats = joystick.get_numhats()
-        print("Number of hats: {}".format(hats) )
-         
+    for i in range( buttons ):
+        button = joystick.get_button( i )
+        print("Button {:>2} value: {}".format(i,button) )
+        
+        
+    # Hat switch. All or nothing for direction, not like joysticks.
+    # Value comes back in an array.
+    hats = joystick.get_numhats()
+    print("Number of hats: {}".format(hats) )
+        
 
-        for i in range( hats ):
-            hat = joystick.get_hat( i )
-            print("Hat {} value: {}".format(i, str(hat)) )
+    for i in range( hats ):
+        hat = joystick.get_hat( i )
+        print("Hat {} value: {}".format(i, str(hat)) )
 
 
     
