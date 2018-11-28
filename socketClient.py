00e50c83f43acada49581e3fbdd1530fd070f234
Nathaniel Bury <nbury@gwu.edu>
2018-11-28 16:16:23 -0500

    Made it be so that you can read the code

--- socketClient.py
+++ socketClient.py
@@ -1,15 +1,22 @@
 import socket
+
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 50000        # The port used by the server
 i=0
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #connect to server
     s.connect((HOST, PORT))
+    #TODO: add try catch to connect 
     while True:
+        #send data
         s.sendall(bytearray([i,i+1]))
+        #data stuff
         i=i+1
         if i>128:
             i=0
+        #recieve data
         data = s.recv(1024)
+        #print data
         if data:  
             print(data[0],data[1])
         else:
--- socketServer.py
+++ socketServer.py
@@ -4,20 +4,26 @@ HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 50000        # Port to listen on (non-privileged ports are > 1023)
 i=0
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #def socket s
     s.bind((HOST, PORT))
     s.listen()
     print("Ready to connect")
+    #wait for connection
     conn, addr = s.accept()
     
     with conn:
+        #on connect being to send and receive data
         print('Connected by', addr)
         while True:
+            #send
             conn.sendall(bytearray([i,i+1]))
+            #ex data
             i=i+1
             if i>128:
                 i=0
+            #then recieve
             data = conn.recv(1024)
-            
+            #print data if received
             if data:  
                 print(data[0],data[1])
             else:
