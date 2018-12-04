1dced2b6c2d177b2f239e9d5c7b7449247f1367a
Nathaniel Bury <nbury@gwu.edu>
2018-12-03 19:17:24 -0500

    Joystick Ready

--- socketClient.py
+++ socketClient.py
@@ -2,23 +2,19 @@ import socket
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 50000        # The port used by the server
-i=0
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     #connect to server
     s.connect((HOST, PORT))
     #TODO: add try catch to connect 
     while True:
         #send data
-        s.sendall(bytearray([i,i+1]))
-        #data stuff
-        i=i+1
-        if i>128:
-            i=0
+        s.sendall(bytearray([1]))
         #recieve data
         data = s.recv(1024)
         #print data
         if data:  
-            print(data[0],data[1])
+            print(data)
         else:
             print('No data received')
         
--- socketServer.py
+++ socketServer.py
@@ -1,8 +1,7 @@
 import socket
-
+#import joystick 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 50000        # Port to listen on (non-privileged ports are > 1023)
-i=0
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     #def socket s
     s.bind((HOST, PORT))
@@ -16,15 +15,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print('Connected by', addr)
         while True:
             #send
-            conn.sendall(bytearray([i,i+1]))
-            #ex data
-            i=i+1
-            if i>128:
-                i=0
+            sendData = joystick.query_values()
+            conn.sendall(bytearray(sendData))
             #then recieve
             data = conn.recv(1024)
             #print data if received
             if data:  
-                print(data[0],data[1])
+                print("I have Data")
             else:
                 print('No data received')
