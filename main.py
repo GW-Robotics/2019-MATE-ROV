61f8010cebb1096a1277a00c2324664bf74f319e
Rick Sear <yobob.sear@gmail.com>
2019-04-02 20:00:46 -0400

    Controls for a single motor

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import socket
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 50000        # The port used by the server
 
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
+
+kit.servo[0].set_pulse_width_range(1000, 2000)
+kit.servo[0].angle = 90
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     #connect to server
     s.connect((HOST, PORT))
@@ -17,6 +23,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             for b in data:
                 new_data.append(b)
             print(new_data)
+            motor_val = map_values(new_data[3], 0, 200, 0, 180)
+            kit.servo[0].angle = int(motor_val)
         else:
             print('No data received')
-        
+
+# Map value x (between a and b) to value y (between c and d)
+def map_values(x, a, b, c, d):
+    return (x-a)*((d-c)/(b-a))+c
