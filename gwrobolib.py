4d519c50e83a72cb532aa66c5a2f0a03914707d7
Rick Sear <yobob.sear@gmail.com>
2019-04-09 20:51:02 -0400

    More features in robolib Motor class

--- gwrobolib.py
+++ gwrobolib.py
@@ -1,19 +1,26 @@
 from adafruit_servokit import ServoKit
+from time import sleep
 class Motor:
     kit = ServoKit(channels=16)
     def __init__(self, pin, angle=90):
         self.pin = pin
         self.angle = angle
-        Motor.kit[pin].set_pulse_width_range(1000, 2000)
-        Motor.kit[pin] = angle
+        self.last_angle = angle
+        Motor.kit.servo[pin].set_pulse_width_range(1000, 2000)
+        Motor.kit.servo[pin].angle = angle
 
     def setSpeed(self, angle):
         self.angle = angle
-        Motor.kit[self.pin] = angle
+        if((angle > 90 and self.last_angle < 90) or (angle < 90 and self.last_angle > 90)):
+            self.stop()
+            sleep(0.1)
+            # print("Changing direction")
+        Motor.kit.servo[self.pin].angle = angle
+        self.last_angle = angle
 
     def stop(self):
         self.angle = 90
-        Motor.kit[self.pin] = angle
+        Motor.kit.servo[self.pin].angle = 90
 
 # class TempSensor: 
 #     def __init__(self, IN1):
