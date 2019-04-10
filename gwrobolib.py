81956ac03cfadc8f3b5dc5bc450ad671d1103d9b
Rick Sear <yobob.sear@gmail.com>
2019-04-09 20:09:29 -0400

    Rewrite motor class in gwrobolib

--- gwrobolib.py
+++ gwrobolib.py
@@ -1,65 +1,31 @@
-from gpiozero import OutputDevice, PWMOutputDevice, InputDevice
-
-class pHsensor:
-    def __init__(self, pin):
-        self.input = InputDevice(pin)
-    
-    def getPh(self):
-        
-        return self.input.value                                     
-
+from adafruit_servokit import ServoKit
 class Motor:
-    def __init__(self, PWM, speed=1):
-        self.speed_pin = PWMOutputDevice(PWM)
-        self.speed = speed
+    kit = ServoKit(channels=16)
+    def __init__(self, pin, angle=90):
+        self.pin = pin
+        self.angle = angle
+        Motor.kit[pin].set_pulse_width_range(1000, 2000)
+        Motor.kit[pin] = angle
 
-    def forward(self):
-        self.speed_pin.value = speed        
-    
-    def backward(self):
-        self.speed_pin.value = (-speed)
-
-    def setSpeed(self, speed):
-        self.speed = speed
+    def setSpeed(self, angle):
+        self.angle = angle
+        Motor.kit[self.pin] = angle
 
     def stop(self):
-        self.speed = 0
+        self.angle = 90
+        Motor.kit[self.pin] = angle
 
-class TempSensor: 
-    def __init__(self, IN1):
-        self.input_1 = InputDevice(IN1)
+# class TempSensor: 
+#     def __init__(self, IN1):
+#         self.input_1 = InputDevice(IN1)
 
-    def getTemp(self): 
-        return self.input_1.value     
+#     def getTemp(self): 
+#         return self.input_1.value     
 
-
-class MotorControl:
-    def __init__(self, frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor):
-        self.frontLeftMotor = frontLeftMotor
-        self.frontRightMotor = frontRightMotor
-        self.backLeftMotor = backLeftMotor
-        self.backRightMotor = backRightMotor
+# class pHsensor:
+#     def __init__(self, pin):
+#         self.input = InputDevice(pin)
     
-    def forward():
-        self.frontLeftMotor.forward()
-        self.frontRightMotor.forward()
-        self.backLeftMotor.backward()
-        self.backRightMotor.backward()
-
-    def backward():
-        self.frontLeftMotor.backward()
-        self.frontRightMotor.backward()
-        self.backLeftMotor.forward()
-        self.backRightMotor.forward()
-    
-    def turnLeft():
-        self.frontRightMotor.forward()
-        self.backLeftMotor.forward()
-        self.frontLeftMotor.stop()
-        self.backRightMotor.stop()
-    
-    def turnRight():
-        self.frontRightMotor.stop()
-        self.backLeftMotor.stop()
-        self.frontLeftMotor.forward()
-        self.backRightMotor.forward()
+#     def getPh(self):
+        
+#         return self.input.value  
